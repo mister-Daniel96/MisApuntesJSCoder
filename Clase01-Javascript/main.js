a551791a49ba56bab4d77516dc3a3e97f2b24f11
Juan Ayala <juandanielayalaquinones@gmail.com>
2023-07-07 14:50:54 -0500

    Clase1

---
+++ Clase01-Javascript/main.js
@@ -0,0 +1,24 @@
+
+console.log("¡MIRÁ MAMÁAA, ESTOY PROGRAMANDO!");
+
+let number=12;
+let oddNumber=57;
+
+console.log(number+oddNumber);
+
+let name=prompt("Cual es su nombre");
+
+console.log(`Bienvenido a nuestra primera web ${name}`);
+
+let multiplication=5*oddNumber;
+
+let olderAge=prompt(`¿${name} es mayor de edad?` );
+let rest=multiplication-100;
+
+console.log(typeof(oddNumber));
+console.log(typeof(name)); 
+
+const añoActual=2022;
+let age=prompt("Ingrese su edad");
+
+alert(`Usten nacio en el año ${añoActual-age}`);
