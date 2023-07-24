0400420ec37d3fba0533dbeeb8ff8ac2820c1b82
Juan Ayala <juandanielayalaquinones@gmail.com>
2023-07-24 12:42:41 -0500

    Corrigiendo

--- PrimeraPre-Entrega/main.js
+++ PrimeraPre-Entrega/main.js
@@ -1,5 +1,5 @@
 //Ejercicio 01
-            /*
+/*
             let correoUsuario = prompt("Ingrese su email");
           let band=false;
             for (let i = 0; i < correoUsuario.length; i++) {
@@ -11,17 +11,42 @@
             else console.log('Es un mail invalido');
             */
 //Ejercicio 02
-            /*
-            let telefonoUsuario = prompt("Ingrese su numero telefonico");
-            let cont = 0;
 
-            for (let i = 0; i < telefonoUsuario.length; i++) cont++;
-            if (cont === 10) alert("Su telefono ingresado es valido");
-            else alert("El telefono ingresado no es valido");
-            */
+//const numberVerificar = /^[0-9]+$/;
+let telefonoUsuario = prompt("Ingrese su numero telefonico");
+let cont = 0;
 
+if (!verificar(telefonoUsuario)) {
+  for (let i = 0; i < telefonoUsuario.length; i++) cont++;
+
+  if (cont === 10) alert("Su telefono ingresado es valido");
+  else alert("El telefono no cumple con los 10 digitos");
+} else alert("El telefono dispone de algun caracter y es incorrecto");
+
+function verificar(numero) {
+  let band = true;
+  for (let i = 0; i < numero.length; i++) {
+    if (
+      numero[i] == "1" ||
+      numero[i] == "2" ||
+      numero[i] == "3" ||
+      numero[i] == "4" ||
+      numero[i] == "5" ||
+      numero[i] == "6" ||
+      numero[i] == "7" ||
+      numero[i] == "8" ||
+      numero[i] == "9" ||
+      numero[i] == "0"
+    ) {
+      band = false;
+    } else band = true;
+
+  }
+  return band;
+}
+console.log(verificar(telefonoUsuario));
 //Ejercicio 03
-                /*
+/*
             let numeroMinimo = prompt("Ingrese el numero minimo");
             let numeroMaximo = prompt("Ingrese el numero maximo");
 
@@ -39,7 +64,7 @@
                 */
 
 //Ejercicio 04
-
+/*
             for(let i=100;i<=250;i++){
                 if(i%2===0){
                     console.log(`
@@ -49,5 +74,4 @@
                 La potencia cuadrada del numero par ${i} es: ${Math.pow(i,2)}`)
               }
             
-
-        
+*/
